09cfce25e91fa448696807a055f221c219cfb24b
Yehonal <yehonal.azeroth@gmail.com>
2019-02-14 16:10:34 +0100

    Updates

--- precache-manifest.57f4f448a66ae7f731d6c86f99c81280.js
+++ precache-manifest.57f4f448a66ae7f731d6c86f99c81280.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/universal-pwa/static/js/runtime~main.d74df484.js"
   },
   {
-    "revision": "52a2a285c0f935baaa10",
-    "url": "/universal-pwa/static/js/main.52a2a285.chunk.js"
+    "revision": "5d18d8d4243727416f5c",
+    "url": "/universal-pwa/static/js/main.5d18d8d4.chunk.js"
   },
   {
     "revision": "15ebed0a94a51bbc7350",
     "url": "/universal-pwa/static/js/1.15ebed0a.chunk.js"
   },
   {
-    "revision": "52a2a285c0f935baaa10",
+    "revision": "5d18d8d4243727416f5c",
     "url": "/universal-pwa/static/css/main.faa30f5d.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/universal-pwa/static/css/1.16bc41e3.chunk.css"
   },
   {
-    "revision": "c98630f158f0bd0d074e82463b828a98",
+    "revision": "b83742a8d3fc9f12053005c432b09423",
     "url": "/universal-pwa/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/universal-pwa/precache-manifest.cfc916193db36519f0bb4e2838cf13a3.js"
+  "/universal-pwa/precache-manifest.57f4f448a66ae7f731d6c86f99c81280.js"
 );
 
 workbox.clientsClaim();
