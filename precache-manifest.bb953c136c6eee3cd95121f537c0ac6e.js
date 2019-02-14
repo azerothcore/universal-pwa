bc7d6467ea4bd7ef669eab650595ced22bdab9b4
Yehonal <yehonal.azeroth@gmail.com>
2019-02-14 15:51:34 +0100

    Updates

---
+++ precache-manifest.bb953c136c6eee3cd95121f537c0ac6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ed0e106f27de0a14708",
+    "url": "/static/js/main.6ed0e106.chunk.js"
+  },
+  {
+    "revision": "15ebed0a94a51bbc7350",
+    "url": "/static/js/1.15ebed0a.chunk.js"
+  },
+  {
+    "revision": "6ed0e106f27de0a14708",
+    "url": "/static/css/main.faa30f5d.chunk.css"
+  },
+  {
+    "revision": "15ebed0a94a51bbc7350",
+    "url": "/static/css/1.16bc41e3.chunk.css"
+  },
+  {
+    "revision": "d91f8de986cfc38821295d4f4d7c56b1",
+    "url": "/index.html"
+  }
+];
