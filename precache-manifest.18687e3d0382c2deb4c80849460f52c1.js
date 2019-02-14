2fa2d627fa9709005917dbef930e2a288dcd64de
Yehonal <yehonal.azeroth@gmail.com>
2019-02-14 19:24:26 +0100

    Updates

---
+++ precache-manifest.18687e3d0382c2deb4c80849460f52c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d74df484760b40d63687",
+    "url": "/universal-pwa/static/js/runtime~main.d74df484.js"
+  },
+  {
+    "revision": "dbeb25fd65f892462f70",
+    "url": "/universal-pwa/static/js/main.dbeb25fd.chunk.js"
+  },
+  {
+    "revision": "851ba4bdbddeaf39d938",
+    "url": "/universal-pwa/static/js/1.851ba4bd.chunk.js"
+  },
+  {
+    "revision": "dbeb25fd65f892462f70",
+    "url": "/universal-pwa/static/css/main.c2409d9c.chunk.css"
+  },
+  {
+    "revision": "851ba4bdbddeaf39d938",
+    "url": "/universal-pwa/static/css/1.49131b3a.chunk.css"
+  },
+  {
+    "revision": "29d0ef8fbb11bb43558972218b151886",
+    "url": "/universal-pwa/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/universal-pwa/precache-manifest.2873ef0628e9f92a493cca9d639ab843.js"
+  "/universal-pwa/precache-manifest.18687e3d0382c2deb4c80849460f52c1.js"
 );
 
 workbox.clientsClaim();
