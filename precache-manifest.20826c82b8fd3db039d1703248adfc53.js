021dbba8bd6bc2317e66dc52a8d0758267d368cb
Yehonal <yehonal.azeroth@gmail.com>
2019-02-15 11:18:55 +0100

    Updates

--- precache-manifest.20826c82b8fd3db039d1703248adfc53.js
+++ precache-manifest.20826c82b8fd3db039d1703248adfc53.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/universal-pwa/static/js/runtime~main.d74df484.js"
   },
   {
-    "revision": "0333760e601c8448c5e5",
-    "url": "/universal-pwa/static/js/main.0333760e.chunk.js"
+    "revision": "35ff5035dc7e6f02d9b5",
+    "url": "/universal-pwa/static/js/main.35ff5035.chunk.js"
   },
   {
     "revision": "d52bbd2a407ebce40174",
     "url": "/universal-pwa/static/js/1.d52bbd2a.chunk.js"
   },
   {
-    "revision": "0333760e601c8448c5e5",
+    "revision": "35ff5035dc7e6f02d9b5",
     "url": "/universal-pwa/static/css/main.7d93c049.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/universal-pwa/static/css/1.49131b3a.chunk.css"
   },
   {
-    "revision": "941634d27a54804d70493f04aea1515e",
+    "revision": "30825e256614ba00325b034a81d45f8d",
     "url": "/universal-pwa/index.html"
   }
 ];
