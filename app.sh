5399805e3f7b18ad74efe82ea872be6077e7be3c
Yehonal <yehonal.azeroth@gmail.com>
2019-04-20 16:32:11 +0200

    upgraded code

--- app.sh
+++ app.sh
@@ -2,16 +2,10 @@
 
 set -e
 
-echo "> Init and updating submodules..."
-[ ! -d "apps/git-subrepo" ] && git submodule add https://github.com/ingydotnet/git-subrepo apps/git-subrepo
-git submodule update --init apps/git-subrepo
-
 ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 
-source "$ROOT_PATH/apps/git-subrepo/.rc"
+source "$ROOT_PATH/apps/git-utils/subrepo.sh"
 
 echo "> Pulling and update all subrepos"
-[ ! -d "src/deps/hw-core/reactstrap-modals" ] && git subrepo clone https://gitlab.com/hw-core/modules/reactstrap-modals/ src/deps/hw-core/reactstrap-modals
-git subrepo pull src/deps/hw-core/reactstrap-modals
-git subrepo push src/deps/hw-core/reactstrap-modals
-git subrepo clean src/deps/hw-core/reactstrap-modals
+
+subrepoUpdate https://gitlab.com/hw-core/modules/reactstrap-modals/ master src/deps/hw-core/reactstrap-modals
